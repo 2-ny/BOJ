920ac796770a41c818f73d80c02d44649c474524
2-ny <georgia6414@gmail.com>
2025-10-26 22:47:30 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 560 ms, Memory: 12084 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void solution(int n, const vector<int>& sequence) {
+	// lis_vec[k]: 길이가 k+1인 증가하는 부분 수열의 마지막 원소 중 가장 작은 값
+	vector<int> lis_vec;
+
+	for(auto element: sequence) {
+		if(lis_vec.empty() || lis_vec.back() < element) {
+			lis_vec.push_back(element);
+		}
+		else {
+			auto it = lower_bound(lis_vec.begin(), lis_vec.end(), element);
+			*it = element;
+		}
+	}
+
+	cout << lis_vec.size() << "\n";
+}
+
+int main() {
+	int n;
+	cin >> n;
+
+	vector<int> sequence(n);
+
+	for(int i = 0; i < n; i++) {
+		cin >> sequence[i];
+	}
+
+	solution(n, sequence);
+
+	return 0;
+}
