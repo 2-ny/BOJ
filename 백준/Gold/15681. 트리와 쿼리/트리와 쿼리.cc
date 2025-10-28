321ad2a507addba6a97c21f5cacb6a8313e06bf0
2-ny <georgia6414@gmail.com>
2025-10-28 11:04:57 +0900

    [Gold V] Title: 트리와 쿼리, Time: 56 ms, Memory: 16012 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.cc
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.cc
@@ -3,17 +3,12 @@
 
 using namespace std;
 
-void solution(int n, int current_node, const vector<vector<int>>& adj, vector<int>& count_node, vector<bool>& visited) {
-	
-	// current_node가 leaf node일 경우(base case)
-	if(adj[current_node].size() == 1 && visited[adj[current_node][0]]) {
-		count_node[current_node] = 1;
-	}
+void count_subtreeSize(int current_node, const vector<vector<int>>& adj, vector<int>& count_node, vector<bool>& visited) {
 
 	for(auto next_node : adj[current_node]) {
 		if(!visited[next_node]) {
 			visited[next_node] = true;
-			solution(n, next_node, adj, count_node, visited);
+			count_subtreeSize(next_node, adj, count_node, visited);
 			count_node[current_node] += count_node[next_node];
 		}
 	}
@@ -46,7 +41,7 @@ int main() {
 	vector<bool> visited(n + 1, false);
 	visited[r] = true;
 
-	solution(n, r, adj, count_node, visited);
+	count_subtreeSize(r, adj, count_node, visited);
 
 	for(auto u : query) {
 		cout << count_node[u] << "\n";
